967894fdd6925413097a21ab27e84c39cfc257b6
Tom Ainsworth <tomainsworth94@gmail.com>
2022-04-30 11:05:25 +0100

    Remove DEBUG

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = "DEVELOPMENT" in os.environ
+
 
 if development:
     ALLOWED_HOSTS = [os.environ.get("localhost")]
